bc69b4bff5e24a28eeda91c073a38594fcaa7c2b
Zehan Khan <zehan9211@gmail.com>
2022-09-23 09:21:22 +0530

    charkra ui provider add and theme set

--- spotify-app/pages/index.tsx
+++ spotify-app/pages/index.tsx
@@ -1,8 +1,9 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import React from 'react'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+const Home = () => {
   return (
     <div className={styles.container}>
       <Head>
@@ -67,3 +68,6 @@ export default function Home() {
     </div>
   )
 }
+
+
+export default Home;
