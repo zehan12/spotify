4afd01928f17026f07d94cc720d70918b3f01fbb
Zehan Khan <zehan9211@gmail.com>
2022-09-28 19:48:38 +0530

    fetcher mutation and form created

---
+++ spotify-app/lib/fetcher.ts
@@ -0,0 +1,9 @@
+export default function fetcher( url: string, data = undefined ){
+    return fetch(`${window.location.origin}/api${url}`,{
+        method: data ? "POST" : "GET",
+        headers: {
+            "Content-Types": "application/json",
+        },
+        body: JSON.stringify(data)
+    });
+}
