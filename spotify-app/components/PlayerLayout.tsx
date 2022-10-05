00723d88dc3fdf061d2f3e41f8acc6497bd1d817
Zehan Khan <zehan9211@gmail.com>
2022-10-05 18:17:46 +0530

    gradient background, image box, title and subtitle UI Layout created

--- spotify-app/components/PlayerLayout.tsx
+++ spotify-app/components/PlayerLayout.tsx
@@ -9,10 +9,8 @@ const PlayerLayout = ({ children }) => {
             <Box position="absolute" top="0" width="250px" left="0">
                 <Sidebar />
             </Box>
-            <Box marginLeft="250px"
-                // marginBottom="100px"
-            >
-                {children}
+            <Box marginLeft="250px" marginBottom="100px">
+                <Box height="calc(100vh - 100px)">{children}</Box>
             </Box>
             <Box position="absolute" left="0" bottom="0">
                 player
--- spotify-app/pages/index.tsx
+++ spotify-app/pages/index.tsx
@@ -1,13 +1,14 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import React from 'react'
+import GradientLayout from '../components/GradientLayouts'
 import styles from '../styles/Home.module.css'
 
 const Home = () => {
   return (
-    <div>
-      Home
-    </div>
+      <GradientLayout color="red" subtitle="profile" title="Zehan Khan" description="15 public playlist" image="https://avatars.githubusercontent.com/u/73664886?v=4" roundImage >
+        <div>Home Page</div>
+      </GradientLayout>
   )
 }
 
